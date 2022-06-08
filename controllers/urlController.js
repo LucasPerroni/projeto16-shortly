f145f5c4887cce478338c3282af5f7c2acddd016
Lucas Bonde Perroni <lucasbondep2@gmail.com>
2022-06-08 15:34:00 -0300

    create database commands

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,4 +1,4 @@
-import db from "../database.js"
+import db from "../database/database.js"
 
 export async function shortenUrl(req, res) {
   try {
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-import db from "../database.js"
+import db from "../database/database.js"
 
 export async function getUser(req, res) {
   try {
