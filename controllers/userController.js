c7d47f47530a49ad493b9b8196eedae2ea29e4c4
Lucas Bonde Perroni <lucasbondep2@gmail.com>
2022-06-09 20:30:50 -0300

    Get user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,38 @@
 import db from "../database/database.js"
 
 export async function getUser(req, res) {
+  const { id } = req.params
+  const { userId } = res.locals
+
+  // validate user based on the token
+  if (userId !== Number(id)) {
+    return res.sendStatus(401)
+  }
+
   try {
-    res.sendStatus(200)
+    // get user data
+    const user = await db.query(`SELECT id, name FROM users WHERE id = $1`, [id])
+
+    // get user urls
+    const urls = await db.query(
+      `
+      SELECT u.id, u."shortUrl", u.url, u."visitCount"
+      FROM urls u 
+      JOIN "userUrls" ON "userUrls"."urlId" = u.id
+      WHERE "userUrls"."userId" = $1
+    `,
+      [id]
+    )
+
+    // count total number of visits
+    let counter = 0
+    urls.rows.forEach(({ visitCount }) => (counter += visitCount))
+
+    // modify user data
+    user.rows[0].visitCount = counter
+    user.rows[0].shortenedUrls = urls.rows
+
+    res.status(200).send(user.rows[0])
   } catch (e) {
     res.sendStatus(500)
   }
--- middlewares/token.js
+++ middlewares/token.js
@@ -6,12 +6,12 @@ dotenv.config()
 export default function token(req, res, next) {
   const { authorization } = req.headers
 
-  try {
-    const token = authorization?.replace("Bearer", "").trim()
-    if (!token) {
-      return res.status(401).send("Token not found")
-    }
+  const token = authorization?.replace("Bearer", "").trim()
+  if (!token) {
+    return res.status(401).send("Token not found")
+  }
 
+  try {
     const user = jwt.verify(token, process.env.JWT_KEY)
 
     res.locals.userId = user.userId
