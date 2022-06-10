a027e928f377b64ffe29e7a9ebd85fd760374ce9
Lucas Bonde Perroni <lucasbondep2@gmail.com>
2022-06-09 23:48:50 -0300

    Ranking of users

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,7 +40,30 @@ export async function getUser(req, res) {
 
 export async function getRank(req, res) {
   try {
-    res.sendStatus(200)
+    // get user ranking based on the count of visits of the user urls
+    // if the visits are the same for two users, then the ranking is based on the count of urls of the user
+    const ranking = await db.query(`
+      SELECT u.id, u.name, COUNT(urls.url) "linksCount", SUM(urls."visitCount") "visitCount"
+      FROM users u
+      LEFT JOIN "userUrls" ON u.id = "userUrls"."userId"
+      LEFT JOIN urls ON urls.id = "userUrls"."urlId"
+      GROUP BY u.name, u.id
+      ORDER BY "visitCount" DESC NULLS LAST, "linksCount" DESC, u.id ASC   
+      LIMIT 10 
+    `)
+
+    // modify "linksCount" and "visitCount" from objects
+    ranking.rows.forEach(({ linksCount, visitCount }, i) => {
+      if (!visitCount) {
+        ranking.rows[i].visitCount = 0
+      } else {
+        ranking.rows[i].visitCount = Number(visitCount)
+      }
+
+      ranking.rows[i].linksCount = Number(linksCount)
+    })
+
+    res.status(200).send(ranking.rows)
   } catch (e) {
     res.sendStatus(500)
   }
